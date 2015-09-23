d00ae104c52fba3ed11194d01ee66f39289f6448
Petri Kortelainen <a1303379@myy.haaga-helia.fi>
2015-09-23 15:25:24 +0300

    Nyt runnaa sovellus =) -Petri

--- robo-app/src/main/java/org/softala/roboapp/model/Hello.java
+++ robo-app/src/main/java/org/softala/roboapp/model/Hello.java
@@ -1,8 +1,18 @@
 package org.softala.roboapp.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="hello")
 public class Hello {
 	//json model yritelma{"id": 1, "content": "Hello, World!"}
+	@Id
 	private final long id;
+	
+	@Column(name="content")
 	private final String content;
 	
 	public Hello(long id, String content){
--- robo-app/src/main/java/org/softala/roboapp/repository/DialogRepository.java
+++ robo-app/src/main/java/org/softala/roboapp/repository/DialogRepository.java
@@ -1,10 +1,11 @@
 package org.softala.roboapp.repository;
 
+import org.softala.roboapp.model.Hello;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface DialogRepository extends CrudRepository<Object, Long> {
+public interface DialogRepository extends CrudRepository<Hello, Long> {
 
 }
