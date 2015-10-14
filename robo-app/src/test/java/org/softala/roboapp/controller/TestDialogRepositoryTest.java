fc0bc8b47d18ca9d75c17e9b25b431653ac9d909
Tuomas Torma <tuomas.torma@myy.haaga-helia.fi>
2015-10-14 15:46:20 +0300

    Adding MIT-License reference, should be checked. Also added new test case

--- robo-app/src/test/java/org/softala/roboapp/controller/TestDialogRepositoryTest.java
+++ robo-app/src/test/java/org/softala/roboapp/controller/TestDialogRepositoryTest.java
@@ -28,14 +28,37 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 /**
  * Test for {@link DialogController} in order to check if the rest controller is active
- * @author a1300786
+ * @author Tuomas Törmä
+ * @since 14.10.2015
+ * 
+ * 
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Haaga-Helia ammattikorkeakoulu Oy ab
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @SpringApplicationConfiguration(classes = App.class)
-@Profile("test")
-public class TestDialogRepository extends TestCase{
+public class TestDialogRepositoryTest extends TestCase{
 	
 	@InjectMocks
 	DialogController dialogController;
