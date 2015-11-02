7ae293e3484bac94cf7e000f9b1a53a3dd298b30
Tuomas Torma <tuomas.torma@myy.haaga-helia.fi>
2015-11-02 09:35:02 +0200

    Export data as CSV

--- robo-app/src/main/java/org/softala/roboapp/controller/ExportCVSController.java
+++ robo-app/src/main/java/org/softala/roboapp/controller/ExportCVSController.java
@@ -1,11 +1,15 @@
 package org.softala.roboapp.controller;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.softala.roboapp.model.GivenAnswer;
+import org.softala.roboapp.repository.GivenAnswerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,6 +45,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/export")
 public class ExportCVSController {
+	
+	@Autowired
+	private GivenAnswerRepository givenAnswersRepository;
 	
 	@RequestMapping(value="/downloadTestCVS")
 	public void downloadTestCVS(HttpServletResponse response){
@@ -79,6 +86,56 @@ public class ExportCVSController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	@RequestMapping("/given_answers")
+	public void downloadGivenAnswersCVS(HttpServletResponse response){
+			//Setting header informaton
+			response.setContentType("text/csv");
+			LocalDate today = LocalDate.now();
+			String reportName = "CVS export Given Answers "+ today.toString();
+			response.setHeader("Content-disposition", "attachment;filename="+reportName);
+		
+			//Adding rows
+			ArrayList<String> rows = new ArrayList<String>();
+			rows.add("Session_id,Dialog_id,Question_text,Given_Anwser_text");
+			rows.add("\n");
+			
+			//Get All given answers from db
+			Iterable<GivenAnswer> answerIterator = givenAnswersRepository.findAll();
+			
+			ArrayList<GivenAnswer> answerList = new ArrayList<>();
+		
+			for (GivenAnswer givenAnswer : answerIterator) {
+				answerList.add(givenAnswer);
+			}
+			
+			for (GivenAnswer givenAnswer : answerList) {
+				rows.add(givenAnswer.getSession().getSessionId()+","+givenAnswer.getAnswerOption().getQuestion().getDialog().getDialogId()+","+ givenAnswer.getAnswerOption().getQuestion().getText() +","+ givenAnswer.getAnswerOption().getText());
+				rows.add(System.lineSeparator());
+			}
+			
+			//foreach line, get the data from arrayList and then
+			//get responses output stream and print data to it
+			Iterator<String> iter = rows.iterator();
+			while (iter.hasNext()) {
+				String outputString = (String) iter.next();
+				try {
+					response.getOutputStream().print(outputString);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+	 
+			try {
+				response.getOutputStream().flush();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
 	}
 
 }
