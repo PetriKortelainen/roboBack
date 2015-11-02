9fe7800659a0e783d2f3f527ef5c2541a94c08ad
unknown <a1300781@PAO5006ZL15.haagahelia.amk>
2015-11-02 11:23:14 +0200

    Gson librabry and utils for converting json to java

---
+++ robo-app/src/main/java/org/softala/roboapp/util/YamlToJsonConverter.java
@@ -0,0 +1,36 @@
+package org.softala.roboapp.util;
+
+import java.util.Map;
+
+import org.json.JSONObject;
+import org.json.simple.JSONValue;
+import org.yaml.snakeyaml.Yaml;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+/**
+ * 
+ * @author Mikko-Ville Salo
+ *
+ *
+ */
+
+public class YamlToJsonConverter {
+	
+	public static String YamlToJsonString(String yamlString){
+		Yaml yaml = new Yaml();
+		Object obj = yaml.load(yamlString);
+		return JSONValue.toJSONString(obj);
+	}
+	
+	public static boolean isValid(String json) {
+	    try {
+	        new JsonParser().parse(json);
+	        return true;
+	    } catch (JsonSyntaxException jse) {
+	        return false;
+	    }
+	}
+}
