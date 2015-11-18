29afd8b1c3d5a2c46dea29ee3689d5de0767e2a1
Tuomas Torma <tuomas.torma@myy.haaga-helia.fi>
2015-11-18 13:31:24 +0200

    some odd error in file

--- robo-app/src/main/java/org/softala/roboapp/repository/DialogRepository.java
+++ robo-app/src/main/java/org/softala/roboapp/repository/DialogRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 
 /**
  * 
- * @author Tuomas Törmä, Kortelainen Petri
+ * @author Tuomas Törmä and Kortelainen Petri
  * @Since 14.10.2015
  * 
  */
